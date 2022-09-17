e329adace7deb975108ebfbf89cee8487d1c82c4
souhir812 <souhir.benboubakar@gmail.com>
2022-09-17 17:21:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Component } from 'react';
+
+
+class App extends Component {
+  render () { 
+  return (
+    <div className="App">
+        <h1 style={{width:"100%",display:"flex",justifyContent:"center"}}> GOOGLE MAPS WITH REACT </h1>
+        <div>
+        <Map google={this.props.google} zoom={14}>
+ 
+ <Marker onClick={this.onMarkerClick}
+         name={'Current location'} />
+
+ <InfoWindow onClose={this.onInfoWindowClose}>
+     
+ </InfoWindow>
+</Map>
+  
+        </div>
+    </div>
+  );
+}}
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDDEnDhn1ILEYyetbsWXaemv_-cKe04yLA")
+})(App);
